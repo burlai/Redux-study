8e83ce31c15b49a6529c8001683258c067487ae8
kostiantyn.burlai <kostiantyn.burlai@makolab.com>
2017-05-12 16:56:39 +0200

    starting

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function () {
+    return [
+        { title: "aasdf asdfasdf" },
+        { title: "213441234" },
+        { title: "kja;lskdjf 12341234" },
+        { title: "65154647984 asdf asdfasdfasdf " }
+    ]
+}
