03eb8a9ce27d8b723033ad199adb29171c90706c
kostiantyn.burlai <kostiantyn.burlai@makolab.com>
2017-05-15 17:02:21 +0200

    video 46 at the middle

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { selectBook } from '../actions/index';
+import { bindActionCreators } from 'redux';
 
 class BookList extends Component {
 
@@ -7,7 +9,11 @@ class BookList extends Component {
         return this.props.books.map(
             (book) => {
                 return(
-                    <li key={book.title} className="list-group-item">{book.title}</li>
+                    <li 
+                        key={book.title} 
+                        onClick={() => this.props.selectBook(book)}
+                        className="list-group-item">{book.title}
+                    </li>
                 );
             }
         );
@@ -24,9 +30,21 @@ class BookList extends Component {
 
 function mapStateToProps(state) {
     console.log(state.books);
+    // Whatever is returned will show up as props
+    // inside of BookList
     return {
         books: state.books
     }
 }
 
-export default connect(mapStateToProps)(BookList);
+// Anything returned from this function will end up as props
+// on the BookList container
+function mapDispatchToProps(dispatch) {
+    // Whenever selectBook is called, the result should be passed
+    // to all of our reducers
+    return bindActionCreators({ selectBook: selectBook }, dispatch);
+}
+
+// Promote BookList from a component to a container - it needs to know
+// about tis new dispatch method, selectBook. Make it available as a prop.
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
---
+++ src/reducers/reducer_active_book.js
@@ -0,0 +1,5 @@
+// State argument is not an application state, but only the state this reducer is responsible for
+export default function(state, action) {
+    
+    return state;
+}
