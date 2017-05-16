467b54716dbdf2e13f63450acd768c8b3d996abf
kostiantyn.burlai <kostiantyn.burlai@makolab.com>
2017-05-16 11:01:36 +0200

    second app finished

--- src/reducers/reducer_active_book.js
+++ src/reducers/reducer_active_book.js
@@ -1,5 +1,10 @@
 // State argument is not an application state, but only the state this reducer is responsible for
-export default function(state, action) {
-    
+export default function(state = null, action) {
+
+    switch(action.type) {
+        case 'BOOK_SELECTED':
+            return action.payload;
+    }
+
     return state;
 }
--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function () {
     return [
-        { title: "aasdf asdfasdf" },
-        { title: "213441234" },
-        { title: "kja;lskdjf 12341234" },
-        { title: "65154647984 asdf asdfasdfasdf " }
+        { title: "aasdf asdfasdf", pages: 123 },
+        { title: "213441234", pages: 643 },
+        { title: "kja;lskdjf 12341234", pages: 543 },
+        { title: "65154647984 asdf asdfasdfasdf ", pages: 1355 }
     ]
 }
